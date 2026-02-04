11693e456044130f0d935fcd92d0566bf292aea5
adelpro <adelpro@gmail.com>
2026-02-04 21:18:15 +0100

    build: update open-quran-view to v0.2.0 and configure pnpm workspace

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    // 👇 Add this. It prevents Vite from moving your library to .vite/deps
+    exclude: ["open-quran-view"],
+  },
 });
